83aab3a615ff14fb4ac0602c210e75783dfc8b6c
sevketcakir <cakiaccounts@gmail.com>
2016-05-02 17:33:30 +0300

    Ctrl+C çıkış oldu, indirme yüzdesi gösteriliyor, hatalı indirmeler hatali.txt dosyasına kaydediliyor

--- cepodcast.py
+++ cepodcast.py
@@ -5,35 +5,53 @@ from subprocess import call
 import sys
 import requests
 import random
-import urllib.request
-import shutil
 import os.path
+from urllib.request import urlretrieve
 
-page=requests.get('http://www.powerfm.com.tr/podcasts/2016/cenk-erdem.html')
-tree=html.fromstring(page.content)
+def reporthook(blocknum, blocksize, totalsize):
+    readsofar = blocknum * blocksize
+    if totalsize > 0:
+        percent = readsofar * 1e2 / totalsize
+        s = "\r%5.1f%% %5.1fM / %5.1fM" % (percent,  readsofar/1048576.0, totalsize/1048576.0)
+        sys.stderr.write(s)
+        if readsofar >= totalsize:  # near the end
+            sys.stderr.write("\n")
+    else:  # total size is unknown
+        sys.stderr.write("read %d\n" % (readsofar,))
+
+
+page = requests.get('http://www.powerfm.com.tr/podcasts/2016/cenk-erdem.html')
+tree = html.fromstring(page.content)
 
 songs = tree.xpath('//div[@class="podcastPlay"]/span/text()')
 
 while True:
-	song = random.choice(songs)
-	print(song)
-	#file_name, headers = urllib.request.urlretrieve(song)
-	file_name=song.split('/')[-1]
-	if not os.path.isfile(file_name):#Dosya mevcut degilse
-		with urllib.request.urlopen(song) as response, open(file_name, 'wb') as out_file:
-			shutil.copyfileobj(response, out_file)
-	print(file_name)
-	try:
-		call(["mplayer", file_name])
-	except:
-		print('mplayer çalıştırılamadı !!!')
-		print('x:Cikis')
-		c = sys.stdin.read(1)
-		if c == 'x':
-			break
-	#print('x:Çıkış')
-	#c = sys.stdin.read(1)
-	#if c == 'x':
-	#	break
+    song = random.choice(songs)
+    print(song)
+    file_name = song.split('/')[-1]
+    if not os.path.isfile(file_name):  # Dosya mevcut degilse
+        try:
+            urlretrieve(song, file_name, reporthook)
+        except KeyboardInterrupt:
+            print('\nÇıkılıyor...')
+            hd = open('hatali.txt',"a")
+            hd.write(file_name+'\n')
+            hd.close()
+            sys.exit()
+        except:
+            print('\nDosya indirmede hata!!')
+            hd = open('hatali.txt',"a")
+            hd.write(file_name+'\n')
+            hd.close()
+
+    print(file_name)
+    try:
+        call(["mplayer", file_name])
+    except:
+        print('mplayer çalıştırılamadı !!!')
+        print('x:Cikis')
+        c = sys.stdin.read(1)
+        if c == 'x':
+            break
 
 
--- playall.py
+++ playall.py
@@ -1,18 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from subprocess import call
 import glob
 import random
 import sys
+import time
 
 songs = glob.glob('./*.mp3')
 print(songs)
 while True:
 	song = random.choice(songs)
-	print(song)
+	print('Çalınan: '+song)
 	try:
 		call(['mplayer',song])
 	except:
 		print('mplayer calistirilamadi')
-	print('x:Cikis')
-	c = sys.stdin.read(1)
-	if c == 'x':
-		break
+	print('Çıkış için Ctrl+C')
+	try:
+		time.sleep(5);
+	except KeyboardInterrupt:
+		print('\nÇıkılıyor...')
+		sys.exit()
+	#print('x:Cikis')
+	#c = sys.stdin.read(1)
+	#if c == 'x':
+	#	break
