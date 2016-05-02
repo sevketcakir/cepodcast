018e92445b06d5355062d21e83488908492916d9
sevketcakir <cakiaccounts@gmail.com>
2016-05-02 18:10:20 +0300

    Ctrl+C ile uygulamadan çıkma, podcast listesini podcasts.txt dosyasından yükleme eklendi.

--- cepodcast.py
+++ cepodcast.py
@@ -7,12 +7,13 @@ import requests
 import random
 import os.path
 from urllib.request import urlretrieve
+import time
 
 def reporthook(blocknum, blocksize, totalsize):
     readsofar = blocknum * blocksize
     if totalsize > 0:
         percent = readsofar * 1e2 / totalsize
-        s = "\r%5.1f%% %5.1fM / %5.1fM" % (percent,  readsofar/1048576.0, totalsize/1048576.0)
+        s = "\r%5.1f%% %5.1fM / %5.1fM" % (percent, readsofar / 1048576.0, totalsize / 1048576.0)
         sys.stderr.write(s)
         if readsofar >= totalsize:  # near the end
             sys.stderr.write("\n")
@@ -20,28 +21,34 @@ def reporthook(blocknum, blocksize, totalsize):
         sys.stderr.write("read %d\n" % (readsofar,))
 
 
-page = requests.get('http://www.powerfm.com.tr/podcasts/2016/cenk-erdem.html')
-tree = html.fromstring(page.content)
+plist = list()  # Tüm yıllardaki kayıtları tutacak liste
+with open("podcastlist.txt") as f:
+    for line in f:
+        if line[0] != '#':
+            page = requests.get(line)
+            tree = html.fromstring(page.content)
+            songs = tree.xpath('//div[@class="podcastPlay"]/span/text()')
+            plist = plist + songs
 
-songs = tree.xpath('//div[@class="podcastPlay"]/span/text()')
+print(len(plist), ' podcast mevcut...\n')
 
 while True:
-    song = random.choice(songs)
+    song = random.choice(plist)
     print(song)
-    file_name = song.split('/')[-1]
+    file_name = 'sound/'+song.split('/')[-1]
     if not os.path.isfile(file_name):  # Dosya mevcut degilse
         try:
             urlretrieve(song, file_name, reporthook)
         except KeyboardInterrupt:
             print('\nÇıkılıyor...')
-            hd = open('hatali.txt',"a")
-            hd.write(file_name+'\n')
+            hd = open('hatali.txt', "a")
+            hd.write(file_name + '\n')
             hd.close()
             sys.exit()
         except:
             print('\nDosya indirmede hata!!')
-            hd = open('hatali.txt',"a")
-            hd.write(file_name+'\n')
+            hd = open('hatali.txt', "a")
+            hd.write(file_name + '\n')
             hd.close()
 
     print(file_name)
@@ -49,9 +56,9 @@ while True:
         call(["mplayer", file_name])
     except:
         print('mplayer çalıştırılamadı !!!')
-        print('x:Cikis')
-        c = sys.stdin.read(1)
-        if c == 'x':
-            break
-
-
+    try:
+        print('Çıkış için Ctrl+C tuşlarına basın...')
+        time.sleep(5)
+    except KeyboardInterrupt:
+        print('Çıkılıyor...')
+        sys.exit()
